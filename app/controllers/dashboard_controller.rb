ab53bc0e3e1fff87badbde5d4f8a04fb96d86da9
cabanga <manucabanga@gmail.com>
2020-01-18 21:03:28 +0100

    config as secções

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,46 @@
 class DashboardController < ApplicationController
+  before_action :set_agrupamento, only: [
+    :admin_agrupamento, 
+    :primeira_seccao, 
+    :segunda_seccao,
+    :terceira_seccao,
+    :quarta_seccao,
+  ]
+
   def index
+    puts "=="*20
+    p params
+    puts "=="*20
   end
+
+  def admin_seccoes
+    
+  end
+  
+  def primeira_seccao
+    
+  end
+  
+  def segunda_seccao
+    
+  end
+
+  def terceira_seccao
+    
+  end
+
+  def quarta_seccao
+    
+  end
+
+  def admin_agrupamento
+    #session[session_admin] = params[:session_admin].blank? ? flase : true
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_agrupamento
+      @agrupamento = Agrupamento.find(params[:agrupamento_id])
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :agrupamentos do
 
+  resources :agrupamentos do  
+    get '/admin_agrupamento', to: 'dashboard#admin_agrupamento', as: 'admin_agrupamento'
+    get '/primeira_seccao', to: 'dashboard#primeira_seccao', as: 'primeira_seccao'
+    get '/segunda_seccao', to: 'dashboard#segunda_seccao', as: 'segunda_seccao'
+    get '/terceira_seccao', to: 'dashboard#terceira_seccao', as: 'terceira_seccao'
+    get '/quarta_seccao', to: 'dashboard#quarta_seccao', as: 'quarta_seccao'
   end
-  
+
+  get '/admin_seccoes', to: 'dashboard#admin_seccoes', as: 'admin_seccoes'
+
   resources :regioes
   root 'dashboard#index'
 end
