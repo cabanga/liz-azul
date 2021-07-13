d3124f2623e12df43a36765beaa1f7d07b1e6cf3
jm <manucabanga@gmail.com>
2021-07-13 16:52:27 +0100

    this commit Add list scouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def scout_year(started_at)
+        DateTime.now.strftime("%Y").to_i - started_at&.strftime("%Y").to_i || 0
+    end
 end
--- app/helpers/scouts_helper.rb
+++ app/helpers/scouts_helper.rb
@@ -1,2 +1,3 @@
 module ScoutsHelper
+
 end
