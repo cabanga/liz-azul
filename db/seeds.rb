4dfc5488407c6ba48d5d9cddf9046bdfd4a854d0
jm <manucabanga@gmail.com>
2021-07-31 10:59:00 +0100

    this commit config project

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(email: "manucabanga@gmail.com", blocked: false, name: "JM Cabanga", role: 222, password: 'q1w2e3r4', password_confirmation: 'q1w2e3r4')
+
+# pg_restore --verbose --clean --no-acl --no-owner -h localhost -U jm -d ag11_development latest.dump
