e18cbea32b53d9602b8f9c6ef18b1a6480e81903
cabanga <manucabanga@gmail.com>
2023-09-24 15:49:07 +0100

    chore: Config packege.json

--- config/application.rb
+++ config/application.rb
@@ -10,14 +10,7 @@ module Cryptonaza
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
