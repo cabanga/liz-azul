5b1522a67fe517d072616c408d4adc9d06446577
cabanga <manucabanga@gmail.com>
2023-09-24 17:45:47 +0100

    chore: Config packege.json

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,7 @@ set :repo_url, "git@github.com:cabanga/liz-azul.git"
 
 set :deploy_to, "/opt/#{fetch :application}"
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+set :default_env, {
+  path: "$HOME/nodejs/bin:$PATH"
+}
